1053d9aa2153e8dc8342eead38e545a4db12ad83
jeromeplepine <jeromeplepine@gmail.com>
2017-04-30 11:49:07 -0400

    new mcc parameters updated

--- ChinookMast.X/headers/CommandFunctions.h
+++ ChinookMast.X/headers/CommandFunctions.h
@@ -33,23 +33,50 @@
 #define MAST_DIR_LEFT               -1.0f
 #define MAST_DIR_RIGHT               1.0f
 
-#define __NEW_MOTOR__
 
 
+/*------------------------------ DC MOTOR ASSEMBLY ----------------------------
+/*
+/* |OUTPUT | <- |GEAR |  <- |MOTOR |  <- |ACTUAL | -> |MOTOR |  -> | ENCODER |
+/* |SHAFT  | <- |BOX  |  <- |SHAFT |  <- |MOTOR  | -> |SHAFT |
+/*
+/*------------------------------ DC MOTOR ASSEMBLY ----------------------------*/
+/* The following #define names are quite misleading, make sure to read the comments
+   before each and every terms */
+
+//#define __NEW_MOTOR__
 
 #ifndef __NEW_MOTOR__
-#define MOTOR_ENCODER_RATIO         49.0f
-#define MAST_MOTOR_RATIO            2450.0f
+/* The following specifications are for the IG42E-49K DC motor.
+   It's also identified as the RB-Cyt-194 on robotshop.com */
 
+#define PULSES_PER_ROTATION         5.0f
+#define MOTOR_ENCODER_RATIO         49.0f
 #define MAST_MOTOR_RATIO            50.0f
-#define MOTOR_DEG_PER_PULSE         360.0f/245.0f
+
+#define MOTOR_DEG_PER_PULSE         360.0f/(PULSES_PER_ROTATION*MOTOR_ENCODER_RATIO)
 
 #else
+/* The following specifications are for the newer 638324 DC motor.
+   It's also identified as the RB-Sct-1012 on robotshop.com */
+
+#define PULSES_PER_ROTATION         12.0f
+
+// MOTOR_ENCODER_RATIO is the ratio between the motor's assembly output
+// shaft and the motor's shaft alone. In other words, it's the gearbox's ratio...
 #define MOTOR_ENCODER_RATIO         71.0f
-#define MAST_MOTOR_RATIO            4260.0f
 
-#define MOTOR_DEG_PER_PULSE         360.0f/852.0f
+// MAST_MOTOR_RATIO is the number of turns the motor's assembly output must
+// perform before the mast gets a full rotation. It's also the amount of teeth
+// present on the cog circling the mast.
+#define MAST_MOTOR_RATIO            60.0f
 
+// MOTOR_DEG_PER_PULSE is obtained by dividing 360degrees by the product
+// of the amount of encoder pulses per motor shaft rotation and the gearbox's
+// ratio. In other words, it's the rotational degrees done by the output shaft
+// per encoder pulse.
+// Ex: 360/( (encoder pulses per motor shaft turns)*(gearbox ratio)
+#define MOTOR_DEG_PER_PULSE         360.0f/(PULSES_PER_ROTATION*MOTOR_ENCODER_RATIO)
 #endif
 
 #define N_DATA_TO_ACQ               300   // Used when debugging with SKADI
